1b62d917bca94892538cc683404acfcb30951aeb
Sprout0321 <mel20564@gmail.com>
2016-01-13 14:37:01 +0800

    大致修改好createLinearLayout1_2_1

--- src/tw/com/ecomuniversal/ecomtest5/SettingActivity.java
+++ src/tw/com/ecomuniversal/ecomtest5/SettingActivity.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
@@ -25,14 +27,14 @@ public class SettingActivity extends Activity {
 	private Activity activity = this;
 	private Context context = this;
 	private ScrollView scrollView1_2;
-	private RelativeLayout relativeLayout1, relativeLayout1_1, relativeLayout1_2_1_n_1;
-	private LinearLayout linearLayout1_2_1, linearLayout1_2_1_n, linearLayout4_4;
-	private ImageView imageView1_1_2, imageView6;
-	private TextView textView1_1_1, textView4_1, textView4_3, textView6;
+	private RelativeLayout relativeLayout1, relativeLayout1_1, relativeLayout1_2_1_n_1, relativeLayout1_2_1_n_1_1;
+	private LinearLayout linearLayout1_2_1, linearLayout1_2_1_n;
+	private ImageView imageView1_1_2, imageView1_2_1_n_1_1_2;
+	private TextView textView1_1_1, textView1_2_1_n, textView1_2_1_n_1_1_3;
 	
 	private final String[] stringArray = {"1","2","3","4","5"};
     private final String[] stringArray2 = {"一", "二", "三", "四"};
-    private Integer itemNumbers4_2 = 0, itemNumbers4_4 = 0;
+    private Integer itemNumbers = 0;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,58 +129,56 @@ public class SettingActivity extends Activity {
 		scrollView1_2.setLayoutParams(scrollViewLayoutParams);
 		
 		createLinearLayout1_2_1();
-//		createLinearLayout1_2_1X();
 	}
 	
 	private void createLinearLayout1_2_1() {
 		linearLayout1_2_1 = new LinearLayout(activity);
 		linearLayout1_2_1.setOrientation(LinearLayout.VERTICAL);
-//		linearLayout1_2_1.setBackgroundColor(0xFFFA5858);
 		LinearLayout.LayoutParams newLayoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
 		linearLayout1_2_1.setLayoutParams(newLayoutParams);
+		
 		scrollView1_2.addView(linearLayout1_2_1);
 		
 		createTextView1_2_1_n("Function1");
 		
-		Integer linearLayout4_2_1Number = stringArray.length/3;
+		Integer linearLayout1_2_1_nNumber = stringArray.length/3;
     	Integer lastImageViewNumber = stringArray.length%3;
-		for (int i = 0; i < linearLayout4_2_1Number; i++) {
-			createLinearLayout1_2_1_n(3);
+		for (int i = 0; i < linearLayout1_2_1_nNumber; i++) {
+			createLinearLayout1_2_1_n(stringArray, 3);
 		}						
 		if (lastImageViewNumber != 0) {
-			createLinearLayout1_2_1_n(lastImageViewNumber);
+			createLinearLayout1_2_1_n(stringArray,lastImageViewNumber);
 		} else {
 			//Do Nothing
 		}
 		createTextView1_2_1_n("Function2");
 		
-		Integer linearLayout4_4_1Number = stringArray2.length/3;
-    	Integer lastImageViewNumber2 = stringArray2.length%3;
-		for (int i = 0; i < linearLayout4_4_1Number; i++) {
-			createLinearLayout4_4(3);
-		}						
-		if (lastImageViewNumber2 != 0) {
-			createLinearLayout4_4(lastImageViewNumber2);
+		linearLayout1_2_1_nNumber = stringArray2.length/3;
+    	lastImageViewNumber = stringArray2.length%3;
+		for (int i = 0; i < linearLayout1_2_1_nNumber; i++) {
+			createLinearLayout1_2_1_n(stringArray2, 3);
+		}
+		if (lastImageViewNumber != 0) {
+			createLinearLayout1_2_1_n(stringArray2,lastImageViewNumber);
 		} else {
 			//Do Nothing
 		}
 	}
 	
 	private void createTextView1_2_1_n(String text) {
-		textView4_1 = new TextView(activity);
+		textView1_2_1_n = new TextView(activity);
 		LayoutParams newLayoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		textView4_1.setLayoutParams(newLayoutParams);
-		textView4_1.setText(text);
-		textView4_1.setTextSize(30f);
-		textView4_1.setTextColor(0xFFF1F2F3);
-//		textView4_1.setBackgroundColor(0xFFFA5858);
-		linearLayout1_2_1.addView(textView4_1);
-    	LinearLayout.LayoutParams textViewLayoutParams = (LinearLayout.LayoutParams)textView4_1.getLayoutParams();
+		textView1_2_1_n.setLayoutParams(newLayoutParams);
+		textView1_2_1_n.setText(text);
+		textView1_2_1_n.setTextSize(30f);
+		textView1_2_1_n.setTextColor(0xFFF1F2F3);
+		linearLayout1_2_1.addView(textView1_2_1_n);
+    	LinearLayout.LayoutParams textViewLayoutParams = (LinearLayout.LayoutParams)textView1_2_1_n.getLayoutParams();
     	textViewLayoutParams.setMargins(20, 20, 0, 20);
-    	textView4_1.setLayoutParams(textViewLayoutParams);
+    	textView1_2_1_n.setLayoutParams(textViewLayoutParams);
 	}
 	
-	private void createLinearLayout1_2_1_n(Integer imageViewNumber) {
+	private void createLinearLayout1_2_1_n(String[] stringArray, Integer imageViewNumber) {
 		linearLayout1_2_1_n = new LinearLayout(activity);
 		linearLayout1_2_1_n.setOrientation(LinearLayout.HORIZONTAL);
 		LinearLayout.LayoutParams newLayoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
@@ -186,93 +186,100 @@ public class SettingActivity extends Activity {
 		linearLayout1_2_1.addView(linearLayout1_2_1_n);
 		
 		for (int i = 0; i < imageViewNumber; i++) {
-			createRelativeLayout1_2_1_n_1(i, 1);
+			createRelativeLayout1_2_1_n_1(i);
 		}
 	}
 	
-	private void createLinearLayout4_4(Integer imageViewNumber) {
-		linearLayout4_4 = new LinearLayout(activity);
-		linearLayout4_4.setOrientation(LinearLayout.HORIZONTAL);
-		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		linearLayout4_4.setLayoutParams(layoutParams);
-		linearLayout1_2_1.addView(linearLayout4_4);
-		
-		for (int i = 0; i < imageViewNumber; i++) {
-			createRelativeLayout1_2_1_n_1(i, 2);
-		}
-	}
-	
-	private void createRelativeLayout1_2_1_n_1(Integer position, Integer group) {
+	private void createRelativeLayout1_2_1_n_1(Integer position) {
 		relativeLayout1_2_1_n_1 = new RelativeLayout(activity);
-//		relativeLayout5.setBackgroundResource(R.drawable.shape_g);
 		Integer width = getRelativeLayout5Width(getScreenWidth());
 		Integer height = width * 4/3;
 		RelativeLayout.LayoutParams newLayoutParams = new RelativeLayout.LayoutParams(width, height);
 		relativeLayout1_2_1_n_1.setLayoutParams(newLayoutParams);
-		
-		createImageView6(position);
-		createTextView6(position, group);
-		createCheckBox6();
-		if (group == 1) {
-			linearLayout1_2_1_n.addView(relativeLayout1_2_1_n_1);			
-		} else {
-			linearLayout4_4.addView(relativeLayout1_2_1_n_1);
-		}
+		createRelativeLayout1_2_1_n_1_1(position);
+		linearLayout1_2_1_n.addView(relativeLayout1_2_1_n_1);
 	}
 
-	private void createImageView6(Integer position) {
-		imageView6 = new ImageView(activity);
+	@SuppressWarnings("deprecation")
+	@SuppressLint("NewApi")
+	private void createRelativeLayout1_2_1_n_1_1(Integer position) {
+		relativeLayout1_2_1_n_1_1 = new RelativeLayout(activity);
+		GradientDrawable border = new GradientDrawable();
+		border.setColor(0x00FFFFFF); // white background
+		border.setStroke(4, 0xFF000000); // black border with full opacity
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+			relativeLayout1_2_1_n_1_1.setBackgroundDrawable(border);
+		} else {
+			relativeLayout1_2_1_n_1_1.setBackground(border);
+		}
+		Integer width = getRelativeLayout1_2_1_n_1_1Width(getScreenWidth());
+		Integer height = getRelativeLayout1_2_1_n_1_1Width(getScreenWidth());
+		RelativeLayout.LayoutParams newLayoutParams = new RelativeLayout.LayoutParams(width, height);
+		newLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, 1);
+		relativeLayout1_2_1_n_1_1.setLayoutParams(newLayoutParams);
+		relativeLayout1_2_1_n_1.addView(relativeLayout1_2_1_n_1_1);
+		
+		createCheckBox1_2_1_n_1_1_1();
+		createImageView1_2_1_n_1_1_2(position);
+		createTextView1_2_1_n_1_1_3(position);
+	}
+	
+	private void createCheckBox1_2_1_n_1_1_1() {
+		CheckBox checkBox = new CheckBox(activity);
+		checkBox.setId(1210111);
+		checkBox.setScaleX(0.8f);
+		checkBox.setScaleY(0.8f);
+		relativeLayout1_2_1_n_1_1.addView(checkBox);
+		RelativeLayout.LayoutParams checkBoxLayoutParams = (RelativeLayout.LayoutParams) checkBox.getLayoutParams();
+		checkBoxLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 1);
+		checkBoxLayoutParams.addRule(RelativeLayout.CENTER_VERTICAL, 1);
+		Integer marginWidth = getCheckBoxMarginWidth(getScreenWidth());
+		checkBoxLayoutParams.setMargins(marginWidth, 0, marginWidth, 0);
+		checkBox.setLayoutParams(checkBoxLayoutParams);
+	}
+
+	private void createImageView1_2_1_n_1_1_2(Integer position) {
+		imageView1_2_1_n_1_1_2 = new ImageView(activity);
 		switch (position) {
 		case 0:
-			imageView6.setBackgroundResource(R.drawable.shape_r);
+			imageView1_2_1_n_1_1_2.setBackgroundResource(R.drawable.shape_r);
 			break;
 		case 1:
-			imageView6.setBackgroundResource(R.drawable.shape_g);
+			imageView1_2_1_n_1_1_2.setBackgroundResource(R.drawable.shape_g);
 			break;
 		case 2:
-			imageView6.setBackgroundResource(R.drawable.shape_b);
+			imageView1_2_1_n_1_1_2.setBackgroundResource(R.drawable.shape_b);
 			break;
 		default:
 			break;
 		}
-		relativeLayout1_2_1_n_1.addView(imageView6);
-		RelativeLayout.LayoutParams imageViewLayoutParams = (RelativeLayout.LayoutParams) imageView6.getLayoutParams();
+		relativeLayout1_2_1_n_1_1.addView(imageView1_2_1_n_1_1_2);
+		RelativeLayout.LayoutParams imageViewLayoutParams = (RelativeLayout.LayoutParams) imageView1_2_1_n_1_1_2.getLayoutParams();
 		imageViewLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, 1);
+		imageViewLayoutParams.addRule(RelativeLayout.RIGHT_OF, 1210111);
 		imageViewLayoutParams.setMargins(0, 0, 0, 0);
     	imageViewLayoutParams.width = getImageView6Width(getScreenWidth());
     	imageViewLayoutParams.height = getImageView6Width(getScreenWidth());
-    	imageView6.setLayoutParams(imageViewLayoutParams);
-    	imageView6.setOnTouchListener(getImageViewOnTouchListener(itemNumbers4_2, imageView6));
+    	imageView1_2_1_n_1_1_2.setLayoutParams(imageViewLayoutParams);
+    	imageView1_2_1_n_1_1_2.setOnTouchListener(getImageViewOnTouchListener(position, imageView1_2_1_n_1_1_2));
 	}
 	
-	private void createTextView6(Integer position, Integer group) {
-		textView6 = new TextView(activity);
-		textView6.setId(1003);
-		if (group == 1) {
-			textView6.setText(stringArray[itemNumbers4_2]);			
-			itemNumbers4_2++;
-		} else {
-			textView6.setText(stringArray2[itemNumbers4_4]);
-			itemNumbers4_4++;
-		}
-    	textView6.setTextSize(30f);
-    	textView6.setTextColor(0xFF9FA0FF);
-    	textView6.setGravity(Gravity.CENTER);
-    	relativeLayout1_2_1_n_1.addView(textView6);
-    	RelativeLayout.LayoutParams textViewLayoutParams = (RelativeLayout.LayoutParams)textView6.getLayoutParams();
+	private void createTextView1_2_1_n_1_1_3(Integer position) {
+		textView1_2_1_n_1_1_3 = new TextView(activity);
+		textView1_2_1_n_1_1_3.setId(1003);
+		textView1_2_1_n_1_1_3.setText(stringArray[itemNumbers]);			
+//		itemNumbers++;
+    	textView1_2_1_n_1_1_3.setTextSize(30f);
+    	textView1_2_1_n_1_1_3.setTextColor(0xFF9FA0FF);
+    	textView1_2_1_n_1_1_3.setGravity(Gravity.CENTER);
+    	relativeLayout1_2_1_n_1_1.addView(textView1_2_1_n_1_1_3);
+    	RelativeLayout.LayoutParams textViewLayoutParams = (RelativeLayout.LayoutParams)textView1_2_1_n_1_1_3.getLayoutParams();
     	textViewLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, 1);
+    	textViewLayoutParams.addRule(RelativeLayout.RIGHT_OF, 1210111);
+    	textViewLayoutParams.width = getImageView6Width(getScreenWidth());
+    	textViewLayoutParams.height = getImageView6Width(getScreenWidth());
     	textViewLayoutParams.setMargins(0, 0, 0, 0);
-    	textView6.setLayoutParams(textViewLayoutParams);	
-	}
-	
-	private void createCheckBox6() {
-		CheckBox checkBox = new CheckBox(activity);
-		relativeLayout1_2_1_n_1.addView(checkBox);
-		RelativeLayout.LayoutParams checkBoxLayoutParams = (RelativeLayout.LayoutParams) checkBox.getLayoutParams();
-		checkBoxLayoutParams.addRule(RelativeLayout.CENTER_VERTICAL, 1);
-		checkBoxLayoutParams.addRule(RelativeLayout.LEFT_OF, 1003);
-		
-		checkBox.setLayoutParams(checkBoxLayoutParams);
+    	textView1_2_1_n_1_1_3.setLayoutParams(textViewLayoutParams);	
 	}
 	
 	@SuppressLint("ClickableViewAccessibility")
@@ -347,12 +354,22 @@ public class SettingActivity extends Activity {
 	}
 	
 	private Integer getRelativeLayout5Width(Integer screenWidth) {
-		Integer imageViewWidth = (screenWidth)/3;
+		Integer imageViewWidth = ((screenWidth)/3);
+		return imageViewWidth;
+	}
+	
+	private Integer getRelativeLayout1_2_1_n_1_1Width(Integer screenWidth) {
+		Integer imageViewWidth = ((screenWidth)/3) - 20;
 		return imageViewWidth;
 	}
 	
 	private Integer getImageView6Width(Integer screenWidth) {
-		Integer imageViewWidth = ((screenWidth)/5) + 40;
+		Integer imageViewWidth = (((screenWidth)/5) - 20);
+		return imageViewWidth;
+	}
+	
+	private Integer getCheckBoxMarginWidth(Integer screenWidth) {
+		Integer imageViewWidth = ((screenWidth)/300);
 		return imageViewWidth;
 	}
 	
